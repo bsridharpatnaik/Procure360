bca9bb44c57e04ea8f36470eb37dede9305c6ead
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2025-05-05 23:44:54 +0530

    Update DatabaseInitializer.java

--- Backend/src/main/java/com/gb/p360/repository/DatabaseInitializer.java
+++ Backend/src/main/java/com/gb/p360/repository/DatabaseInitializer.java
@@ -5,8 +5,10 @@ import com.gb.p360.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.core.env.Environment;
 
 import java.util.Optional;
+import java.util.Arrays;
 
 @Component
 public class DatabaseInitializer implements CommandLineRunner {
@@ -14,8 +16,18 @@ public class DatabaseInitializer implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private Environment environment;
+
     @Override
     public void run(String... args) throws Exception {
+
+        //return if non-local profile detected.
+        if (Arrays.stream(environment.getActiveProfiles()).noneMatch("local"::equalsIgnoreCase)) {
+            System.out.println("Non-Local Profile found. Return without database initialization");
+            return;
+        }
+
         Optional<User> factoryOptional = userRepository.findByUsername("factory");
         if (!factoryOptional.isPresent()) {
             User raja = new User();
